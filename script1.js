9d7548e43ef745be1f642057401e3ef316e771fb
Ranjith Kumar S <118428398+ranjith-nsrk@users.noreply.github.com>
2023-02-10 19:59:37 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,51 @@
+Here is a sample JSON format for a resume:
+
+
+let myResume={
+    "basics": {
+      "name": "Ranjith Kumar S",
+      "email": "sranjithkumar.nsrk@gamil.com",
+      "phone": 9345880054,
+      "degree": "B.E",
+      "location": {
+        "address": "2,Madathu Street",
+        "postalCode": 626101,
+        "city": "Aruppukottai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "www.linkedin.com/in/ranjith-kumar-s-638222251",
+          "github":"https://github.com/ranjith-nsrk"
+        }
+      ]
+    },
+  "education": [
+      {
+        "institution": "KLNCE",
+        "department": "Automobile",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "gpa": 7.7,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,html,css",
+        "level": "beginer",
+       }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Bike,games,",
+      }
+    ]
+  }
+  console.log(myResume);
